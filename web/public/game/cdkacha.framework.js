83464a3a0d1dc50accafc0eda34de9e0603a0335
Nico <nicholas.computacao@outlook.com>
2022-03-03 23:05:06 +0000

    feat: updated version of Cdkacha game on React App

--- web/public/game/cdkacha.framework.js
+++ web/public/game/cdkacha.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2302784;
+STATICTOP = STATIC_BASE + 2302800;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3362,7 +3362,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2302784;
+var STATIC_BUMP = 2302800;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
